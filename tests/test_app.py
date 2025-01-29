443fe494a1e68120094f463c1457a328f773582d
Richter888 <mulka.marcin@gmail.com>
2025-01-29 22:40:36 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,9 @@
-def hello_world():
-    return "Hello, CI/CD!"
+import unittest
+from app import hello_world  # Zakładając, że masz funkcję hello_world w app.py
+
+class TestApp(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, CI/CD!")
+
+if __name__ == '__main__':
+    unittest.main()
